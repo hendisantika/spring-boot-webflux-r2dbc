47e6ab4b3c66aa1e8b2d55d56ffc46ad24f02ca6
hendisantika <hendisantika@yahoo.co.id>
2024-01-15 10:20:28 +0700

    Add getPrivateKey in JwtKeyUtil🫸🌀✏️📗 :octocat:

--- src/main/java/id/my/hendisantika/webfluxr2dbc/util/JwtKeyUtil.java
+++ src/main/java/id/my/hendisantika/webfluxr2dbc/util/JwtKeyUtil.java
@@ -3,6 +3,13 @@ package id.my.hendisantika.webfluxr2dbc.util;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import java.security.Key;
+import java.security.KeyFactory;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.util.Base64;
+
 /**
  * Created by IntelliJ IDEA.
  * Project : spring-boot-webflux-r2dbc
@@ -18,4 +25,16 @@ public class JwtKeyUtil {
 
     @Value("${application.security.jwt.secretkey}")
     private String secretKey;
+
+    public Key getPrivateKey() {
+        try {
+            byte[] privateKeyBytes = Base64.getDecoder().decode(secretKey);
+            PKCS8EncodedKeySpec spec = new PKCS8EncodedKeySpec(privateKeyBytes);
+            KeyFactory kf = KeyFactory.getInstance("RSA");
+            return kf.generatePrivate(spec);
+
+        } catch (NoSuchAlgorithmException | InvalidKeySpecException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
