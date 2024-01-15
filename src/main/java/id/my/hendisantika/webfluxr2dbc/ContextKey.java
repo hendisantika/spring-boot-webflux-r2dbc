cda87b3c3f8b806ecc3cb65b136541f132474d39
hendisantika <hendisantika@yahoo.co.id>
2024-01-15 10:23:43 +0700

    Add ContextKey🫸🌀✏️📗 :octocat:

---
+++ src/main/java/id/my/hendisantika/webfluxr2dbc/ContextKey.java
@@ -0,0 +1,15 @@
+package id.my.hendisantika.webfluxr2dbc;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-webflux-r2dbc
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 1/15/24
+ * Time: 10:23
+ * To change this template use File | Settings | File Templates.
+ */
+public class ContextKey {
+    public static final String AUTHORIZATION = "authorization";
+}
