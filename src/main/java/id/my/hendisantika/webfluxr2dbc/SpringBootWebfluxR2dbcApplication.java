3c3dfd06504c4aab555732795f3711d4592e7086
hendisantika <hendisantika@yahoo.co.id>
2024-01-15 10:10:00 +0700

    Create starter project 🫸🌀✏️📗 :octocat:

---
+++ src/main/java/id/my/hendisantika/webfluxr2dbc/SpringBootWebfluxR2dbcApplication.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.webfluxr2dbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebfluxR2dbcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootWebfluxR2dbcApplication.class, args);
+    }
+
+}
