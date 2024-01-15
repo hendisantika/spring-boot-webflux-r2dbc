135df565a78d5f4ca6fd64f717f531df97a242dd
hendisantika <hendisantika@yahoo.co.id>
2024-01-15 11:42:36 +0700

    Setup Swagger UI🫸🌀✏️📗 :octocat:

--- src/main/java/id/my/hendisantika/webfluxr2dbc/SpringBootWebfluxR2dbcApplication.java
+++ src/main/java/id/my/hendisantika/webfluxr2dbc/SpringBootWebfluxR2dbcApplication.java
@@ -2,8 +2,12 @@ package id.my.hendisantika.webfluxr2dbc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
+import org.springframework.web.reactive.config.EnableWebFlux;
 
 @SpringBootApplication
+@EnableWebFlux
+@EnableR2dbcRepositories
 public class SpringBootWebfluxR2dbcApplication {
 
     public static void main(String[] args) {
--- src/main/java/id/my/hendisantika/webfluxr2dbc/config/SwaggerConfig.java
+++ src/main/java/id/my/hendisantika/webfluxr2dbc/config/SwaggerConfig.java
@@ -4,6 +4,12 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.License;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /**
@@ -25,4 +31,30 @@ import org.springframework.context.annotation.Configuration;
         scheme = "bearer"
 )
 public class SwaggerConfig {
+    @Bean
+    public GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+                .group("base-service")
+                .pathsToMatch("/**")
+                .build();
+    }
+
+    @Bean
+    public OpenAPI customOpenAPI(@Value("${application-description}") String appDescription, @Value("${application" +
+            "-version}") String appVersion) {
+        Contact contact = new Contact();
+        contact.setEmail("hendisantika@yahoo..co.id");
+        contact.setName("HENDI SANTIKA");
+        contact.setUrl("https://www.s.id/hendisantika");
+        return new OpenAPI()
+                .info(new io.swagger.v3.oas.models.info.Info()
+                        .title("Spring Boot WebFlux R2DBC Service Application API")
+                        .version(appVersion)
+                        .description(appDescription)
+                        .termsOfService("http://swagger.io/terms/")
+                        .license(new License().name("Apache 2.0").url("http://springdoc.org"))
+                        .contact(contact)
+                );
+
+    }
 }
