7f7ccf499e92539cb70d6ce6e172e4314382d828
hendisantika <hendisantika@yahoo.co.id>
2024-01-15 10:29:37 +0700

    Add DatabaseConfig🫸🌀✏️📗 :octocat:

---
+++ src/main/java/id/my/hendisantika/webfluxr2dbc/config/DatabaseConfig.java
@@ -0,0 +1,33 @@
+package id.my.hendisantika.webfluxr2dbc.config;
+
+import io.r2dbc.spi.ConnectionFactory;
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.r2dbc.connection.init.ConnectionFactoryInitializer;
+import org.springframework.r2dbc.connection.init.ResourceDatabasePopulator;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-webflux-r2dbc
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 1/15/24
+ * Time: 10:29
+ * To change this template use File | Settings | File Templates.
+ */
+@Configuration
+@AllArgsConstructor
+public class DatabaseConfig {
+
+    @Bean
+    ConnectionFactoryInitializer initializer(ConnectionFactory connectionFactory) {
+        ConnectionFactoryInitializer initializer = new ConnectionFactoryInitializer();
+        initializer.setConnectionFactory(connectionFactory);
+        initializer.setDatabasePopulator(new ResourceDatabasePopulator(new ClassPathResource("schema.sql")));
+
+        return initializer;
+    }
+}
