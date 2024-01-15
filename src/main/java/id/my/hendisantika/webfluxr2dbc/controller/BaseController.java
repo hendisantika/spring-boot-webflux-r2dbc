db563648a9d9feebd3a2c22652ee9673d2d70a57
hendisantika <hendisantika@yahoo.co.id>
2024-01-15 10:41:10 +0700

    Add BaseController🫸🌀✏️📗 :octocat:

---
+++ src/main/java/id/my/hendisantika/webfluxr2dbc/controller/BaseController.java
@@ -0,0 +1,17 @@
+package id.my.hendisantika.webfluxr2dbc.controller;
+
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-webflux-r2dbc
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 1/15/24
+ * Time: 10:40
+ * To change this template use File | Settings | File Templates.
+ */
+@SecurityRequirement(name = "bearerAuth")
+public class BaseController {
+}
