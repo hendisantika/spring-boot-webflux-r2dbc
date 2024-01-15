3693062d4d07ea8a9370d6d2cbf1db1ada070214
hendisantika <hendisantika@yahoo.co.id>
2024-01-15 10:17:21 +0700

    Add Role Enum 🫸🌀✏️📗 :octocat:

---
+++ src/main/java/id/my/hendisantika/webfluxr2dbc/entity/Role.java
@@ -0,0 +1,15 @@
+package id.my.hendisantika.webfluxr2dbc.entity;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-webflux-r2dbc
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 1/15/24
+ * Time: 10:17
+ * To change this template use File | Settings | File Templates.
+ */
+public enum Role {
+    ROLE_USER, ROLE_ADMIN
+}
