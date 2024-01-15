a767db26dee044ecb1c5ac76ba72554d99c53a9f
hendisantika <hendisantika@yahoo.co.id>
2024-01-15 10:22:30 +0700

    Add getPhone in JwtValidationUtil🫸🌀✏️📗 :octocat:

--- src/main/java/id/my/hendisantika/webfluxr2dbc/util/JwtValidationUtil.java
+++ src/main/java/id/my/hendisantika/webfluxr2dbc/util/JwtValidationUtil.java
@@ -33,4 +33,8 @@ public class JwtValidationUtil {
                 .parseClaimsJws(token)
                 .getBody();
     }
+
+    public String getPhone(String token) {
+        return getClaimsToken(token).getSubject();
+    }
 }
