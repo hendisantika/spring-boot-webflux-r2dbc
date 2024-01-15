b32ee40a5a40dc6aeb0b23f1c6698598b79522c6
hendisantika <hendisantika@yahoo.co.id>
2024-01-15 10:31:49 +0700

    Add LoginResponse🫸🌀✏️📗 :octocat:

---
+++ src/main/java/id/my/hendisantika/webfluxr2dbc/dto/LoginResponse.java
@@ -0,0 +1,24 @@
+package id.my.hendisantika.webfluxr2dbc.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-webflux-r2dbc
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 1/15/24
+ * Time: 10:31
+ * To change this template use File | Settings | File Templates.
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+public class LoginResponse {
+    private String token;
+}
