1ebeb5b8690a25390de4820d39e589bf20556852
hendisantika <hendisantika@yahoo.co.id>
2024-01-15 10:29:03 +0700

    Add SwaggerConfig🫸🌀✏️📗 :octocat:

---
+++ src/main/java/id/my/hendisantika/webfluxr2dbc/config/SwaggerConfig.java
@@ -0,0 +1,28 @@
+package id.my.hendisantika.webfluxr2dbc.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-webflux-r2dbc
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 1/15/24
+ * Time: 10:28
+ * To change this template use File | Settings | File Templates.
+ */
+@Configuration
+@OpenAPIDefinition(info = @Info(title = "CRUD API DEMO WEBFLUX", version = "v1"))
+@SecurityScheme(
+        name = "bearerAuth",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        scheme = "bearer"
+)
+public class SwaggerConfig {
+}
